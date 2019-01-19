75586b02ed20d17cd572fff2fe3411b8a3a8e85c
B230ft <igorniyazov@gmail.com>
2019-01-19 07:42:58 -0800

    added server file along with changes to html

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+    {
+      name: "Russell Wilson",
+      photo: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/14881.png&w=350&h=254 ",
+      scores: ["2", "2", "2", "2", "2"]
+    }, {
+      name: "Aaron Judge",
+      photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
+      scores: ["3", "3", "3", "3", "3"]
+    }, {
+      name: " Stephen Curry",
+      photo: "https://i.kinja-img.com/gawker-media/image/upload/s--S8H4zPqx--/c_scale,f_auto,fl_progressive,q_80,w_800/wfjk2nrdqurtnow287kd.jpg ",
+      scores: ["3", "3", "3", "3", "1"]
+    }, {
+      name: "Lebron James",
+      photo: "https://s22928.pcdn.co/wp-content/uploads/2018/12/LeBron-James-31-640x454.jpeg ",
+      scores: ["3", "3", "3", "3", "1"]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
